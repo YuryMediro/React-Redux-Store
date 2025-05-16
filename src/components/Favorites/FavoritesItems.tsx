4b0eb3b3530a05e6951180e84d7ac32c862abc9a
Yury Mediro <sagn375@gmail.com>
2025-05-16 16:04:19 +0300

    favorites page

--- src/components/Favorites/FavoritesItems.tsx
+++ src/components/Favorites/FavoritesItems.tsx
@@ -19,49 +19,65 @@ export const FavoritesItems = ({
 	handleAddToCart,
 }: FavoritesItemsProps) => {
 	return (
-		<div key={`${item.id}-${item.size}`} className={s.cartItem}>
-			<NavLink to={`/products/${item.id}`}>
-				<img
-					src={item.images[0]}
-					alt={item.title}
-					className={s.img}
-					onError={e => {
-						const target = e.target as HTMLImageElement
-						target.src = noImage
-					}}
-				/>
-			</NavLink>
-			<div className={s.productInfo}>
-				<div className={s.description}>
-					<p className={s.productName}>{item.title}</p>
-					<div className={s.productCategorySize}>
-						<p className={s.productCategory}>{item.category.name}</p>
-						<p className={s.size}>size: {item.size}</p>
+		<div className={s.wrapper}>
+			<div key={`${item.id}-${item.size}`} className={s.cartItem}>
+				<NavLink to={`/products/${item.id}`}>
+					<img
+						src={item.images[0]}
+						alt={item.title}
+						className={s.img}
+						onError={e => {
+							const target = e.target as HTMLImageElement
+							target.src = noImage
+						}}
+					/>
+				</NavLink>
+				<div className={s.productInfo}>
+					<div className={s.description}>
+						<p className={s.productName}>{item.title}</p>
+						<div className={s.productCategorySize}>
+							<p className={s.productCategory}>{item.category.name}</p>
+							<p className={s.size}>size: {item.size}</p>
+						</div>
+					</div>
+					<div className={s.priceQuantity}>
+						<p className={s.price}>{item.price}$</p>
+
+						{inCart ? (
+							<NavLink to={'/shoppingCart'}>
+								<button className={s.inCart}>In Cart</button>
+							</NavLink>
+						) : (
+							<Button
+								onClick={() => handleAddToCart(item, 1)}
+								className={s.addToCart}
+							>
+								Add to Cart
+							</Button>
+						)}
+
+						<button
+							onClick={() => handleRemove(item.id, item.size)}
+							className={s.remove}
+						>
+							<img src={cross} alt={'cross'} />
+						</button>
 					</div>
 				</div>
-				<div className={s.priceQuantity}>
-					<p className={s.price}>{item.price}$</p>
-
-					{inCart ? (
-						<NavLink to={'/shoppingCart'}>
-							<button className={s.inCart}>In Cart</button>
-						</NavLink>
-					) : (
-						<Button
-							onClick={() => handleAddToCart(item, 1)}
-							className={s.button}
-						>
-							Add to Cart
-						</Button>
-					)}
-
-					<button
-						onClick={() => handleRemove(item.id, item.size)}
-						className={s.remove}
+			</div>
+			<div className={s.buttonMobile}>
+				{inCart ? (
+					<NavLink to={'/shoppingCart'}>
+						<button className={s.inCartMobile}>In Cart</button>
+					</NavLink>
+				) : (
+					<Button
+						onClick={() => handleAddToCart(item, 1)}
+						className={s.addToCartMobile}
 					>
-						<img src={cross} alt={'cross'} />
-					</button>
-				</div>
+						Add to Cart
+					</Button>
+				)}
 			</div>
 		</div>
 	)
